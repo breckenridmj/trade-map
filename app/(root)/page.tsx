acf48f046eff1acabce8c8b507e60e425d14f7ac
Malcolm Breckenridge <breckenridmj@vcu.edu>
2023-09-18 21:04:59 -0500

    added clerk auth

---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+      <div>
+        <p>TradeMap Dashboard</p>
+        <Button>Click Me</Button>
+      </div>
+  )
+}
